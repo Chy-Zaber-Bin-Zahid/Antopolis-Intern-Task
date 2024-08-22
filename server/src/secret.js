06f1d2735f7291e352d156ddb8b942157c680e15
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2024-08-22 17:47:59 +0600

    first commit

---
+++ server/src/secret.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const serverPort = process.env.SERVER_PORT || 3002;
+const mongodbURL =
+  process.env.MONGODB_ATLAS_URL || "mongodb://localhost:27017/Antopolis";
+
+module.exports = {
+  serverPort,
+  mongodbURL,
+};
